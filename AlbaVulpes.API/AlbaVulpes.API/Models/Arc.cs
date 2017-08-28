927a727d4852b9cefbf780cb0bdee4775d1fd540
Luis Felipe Hernandez <luis.hernandez093@gmail.com>
2017-08-28 17:17:40 -0400

    Added model classes

---
+++ AlbaVulpes.API/AlbaVulpes.API/Models/Arc.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AlbaVulpes.API.Models
+{
+    public class Arc
+    {
+        public int Number { get; set; }
+        public string Title { get; set; }
+    }
+}
---
+++ AlbaVulpes.API/AlbaVulpes.API/Models/Page.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AlbaVulpes.API.Models
+{
+    public class Page
+    {
+        public int Number { get; set; }
+        public string ImageId { get; set; }
+    }
+}
