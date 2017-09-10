04ea095750425a13607d7acebc967b303bca4ae4
cypherix93 <bikram.ghosh93@gmail.com>
2017-09-10 13:55:13 -0400

    Added unit of work container.

---
+++ src/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿using AlbaVulpes.API.Base;
+using Marten;
+
+namespace AlbaVulpes.API.Interfaces
+{
+    public interface IUnitOfWork
+    {
+        IDocumentStore GetStore();
+        ApiRepository<TModel> GetRepository<TModel>() where TModel : ApiModel;
+        TRepository GetRepository<TModel, TRepository>() where TModel : ApiModel where TRepository : ApiRepository<TModel>;
+    }
+}
