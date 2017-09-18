148d23c489f7e4728acd86e75214bd4e857fbd07
cypherix93 <bikram.ghosh93@gmail.com>
2017-09-17 22:25:14 -0400

    Few tweaks.

--- src/Extensions/ServiceCollectionExtensions.cs
+++ src/Extensions/ServiceCollectionExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using AlbaVulpes.API.Interfaces;
+using AlbaVulpes.API.Models.Resource;
 using AlbaVulpes.API.Repositories;
 using Marten;
 using Microsoft.Extensions.DependencyInjection;
@@ -10,7 +11,10 @@ namespace AlbaVulpes.API.Extensions
         public static void AddMarten(this IServiceCollection services)
         {
             services.AddScoped<IDocumentStore>(provider =>
-                DocumentStore.For("host=localhost;port=5432;database=AlbaVulpes;username=albavulpes;password=asdfghjkl;")
+                DocumentStore.For(_ =>
+                {
+                    _.Connection("host=localhost;port=5432;database=AlbaVulpes;username=albavulpes;password=asdfghjkl");
+                })
             );
         }
 
