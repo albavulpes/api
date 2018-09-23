cdb55c754639fde66fe029c9cf3e87a4ebebd603
cypherix93 <bikram.ghosh93@gmail.com>
2018-09-22 21:10:47 -0400

    Added description field for comic.

--- src/Models/Resource/Comic.cs
+++ src/Models/Resource/Comic.cs
@@ -10,6 +10,7 @@ namespace AlbaVulpes.API.Models.Resource
     {
         public string Title { get; set; }
         public string Author { get; set; }
+        public string Description { get; set; }
         public DateTime ReleaseDate { get; set; }
         public Image CoverImage { get; set; }
     }
