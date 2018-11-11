0c643a7adead51283801ee44879abd8393dfd113
cypherix93 <bikram.ghosh93@gmail.com>
2018-11-10 20:11:32 -0500

    Added user and role models

---
+++ src/Models/Identity/User.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace AlbaVulpes.API.Models.Identity
+{
+    public class User
+    {
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        public List<Role> Roles { get; set; }
+    }
+}
