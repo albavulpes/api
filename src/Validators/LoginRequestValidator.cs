b79f3ebd187cda8f6ce82e1fa534fecdff5df6bb
cypherix93 <bikram.ghosh93@gmail.com>
2018-11-10 22:17:00 -0500

    Added login and register validators

---
+++ src/Validators/LoginRequestValidator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using AlbaVulpes.API.Models.Requests;
+using FluentValidation;
+
+namespace AlbaVulpes.API.Validators
+{
+    public class LoginRequestValidator : AbstractValidator<LoginRequest>
+    {
+        public LoginRequestValidator()
+        {
+            RuleFor(x => x.Email).NotEmpty();
+            RuleFor(x => x.Password).NotEmpty();
+        }
+    }
+}
