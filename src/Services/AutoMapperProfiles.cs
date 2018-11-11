b7d83f5869cda70f3e9683fcb1f1dcc68a424892
cypherix93 <bikram.ghosh93@gmail.com>
2018-11-11 16:02:15 -0500

    Using automapper extensions

---
+++ src/Services/AutoMapperProfiles.cs
@@ -0,0 +1,16 @@
+﻿using AlbaVulpes.API.Models.Resource;
+using AlbaVulpes.API.Models.View;
+using AutoMapper;
+
+namespace AlbaVulpes.API.Services
+{
+    public class AutomapperProfile : Profile
+    {
+        public AutomapperProfile()
+        {
+            CreateMap<Comic, ComicViewModel>();
+            CreateMap<Arc, ArcViewModel>();
+            CreateMap<Chapter, ChapterViewModel>();
+        }
+    }
+}
