583d6554b949b5c910f0a50c96874f1aca6ce738
cypherix93 <bikram.ghosh93@gmail.com>
2018-11-22 15:50:20 -0500

    Config from appsettings

--- src/Extensions/MartenExtensions.cs
+++ src/Extensions/MartenExtensions.cs
@@ -1,16 +1,23 @@
-﻿using Marten;
+﻿using AlbaVulpes.API.Models.Config;
+using Marten;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Npgsql;
 
 namespace AlbaVulpes.API.Extensions
 {
     public static class MartenExtensions
     {
-        public static void AddMarten(this IServiceCollection services)
+        public static void AddMarten(this IServiceCollection services, IConfiguration config)
         {
+            // configure strongly typed settings objects
+            var appSettings = config.GetSection("AppSettings").Get<AppSettings>();
+            var dbConfig = appSettings.Database;
+
             services.AddScoped<IDocumentStore>(provider =>
                 DocumentStore.For(options =>
                 {
-                    options.Connection("host=localhost;port=5432;database=AlbaVulpes;username=albavulpes;password=asdfghjkl");
+                    options.Connection($"host={dbConfig.Host};port={dbConfig.Port};database={dbConfig.Name};username={dbConfig.Username};password={dbConfig.Password}");
                 })
             );
         }
--- src/Models/Config/AppSettings.cs
+++ src/Models/Config/AppSettings.cs
@@ -3,5 +3,16 @@
     public class AppSettings
     {
         public string AuthCookieName { get; set; }
+
+        public DatabaseSettings Database { get; set; }
+    }
+
+    public class DatabaseSettings
+    {
+        public string Host { get; set; }
+        public string Port { get; set; }
+        public string Name { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
     }
 }
