9452846182fec9f85a70353fc938282890053edb
cypherix93 <bikram.ghosh93@gmail.com>
2019-01-26 18:45:51 -0500

    Added image processing logic

---
+++ src/Controllers/ImageController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using AlbaVulpes.API.Base;
+using AlbaVulpes.API.Constants;
+using AlbaVulpes.API.Modules.Images;
+using AlbaVulpes.API.Services;
+using AlbaVulpes.API.Services.AWS;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Internal;
+
+namespace AlbaVulpes.API.Controllers
+{
+    [Route("images")]
+    [Produces("application/json")]
+    public class ImageController : ApiController
+    {
+        private readonly IS3UploadService _s3UploadService;
+
+        public ImageController(IUnitOfWork unitOfWork, IValidatorService validator, IS3UploadService s3UploadService) : base(unitOfWork, validator)
+        {
+            _s3UploadService = s3UploadService;
+        }
+
+        [Authorize(Roles = "Creator")]
+        [HttpPost]
+        public async Task<IActionResult> Create(IFormFile file, bool createThumbnail = false)
+        {
+            if (file == null)
+            {
+                return BadRequest("File not provided or unreadable");
+            }
+
+            var contentType = file.ContentType;
+            var supportedFileTypes = new[] { "image/bmp", "image/jpg", "image/jpeg", "image/png", "image/gif" };
+
+            if (!supportedFileTypes.Contains(contentType))
+            {
+                return BadRequest($"Content Type {contentType} not supported");
+            }
+
+            string originalImageUrl = null;
+            string thumbnailImageUrl = null;
+
+            using (var imageFileStream = new MemoryStream())
+            {
+                await file.CopyToAsync(imageFileStream);
+
+                using (var processedImageStream = ImageProcessor.ProcessImage(imageFileStream, contentType))
+                {
+                    var fileKey = $"{S3StorageOptions.ImageUploadsKeyPrefix}/image-{Guid.NewGuid()}.jpg";
+
+                    originalImageUrl = await _s3UploadService.UploadFileAsync(fileKey, processedImageStream);
+
+                    if (createThumbnail)
+                    {
+                        using (var thumbnailImageStream = ImageProcessor.CreateThumbnail(processedImageStream, contentType))
+                        {
+                            var keyWithoutExt = Path.GetFileNameWithoutExtension(fileKey);
+                            var thumbnailFileKey = fileKey.Replace(keyWithoutExt, keyWithoutExt + "-thumbnail");
+
+                            thumbnailImageUrl = await _s3UploadService.UploadFileAsync(thumbnailFileKey, thumbnailImageStream);
+                        }
+                    }
+                }
+            }
+
+            return Ok((
+                MainImage: originalImageUrl,
+                ThumbnailImage: thumbnailImageUrl
+            ));
+        }
+    }
+}
---
+++ src/Modules/Images/ImageProcessor.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.IO;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Formats;
+using SixLabors.ImageSharp.Formats.Bmp;
+using SixLabors.ImageSharp.Formats.Gif;
+using SixLabors.ImageSharp.Formats.Jpeg;
+using SixLabors.ImageSharp.Formats.Png;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing;
+using SixLabors.ImageSharp.Processing.Processors.Transforms;
+
+namespace AlbaVulpes.API.Modules.Images
+{
+    public static class ImageProcessor
+    {
+        public static Stream ProcessImage(Stream inStream, string contentType = null)
+        {
+            inStream.Position = 0;
+
+            using (var image = LoadImage(inStream, contentType))
+            {
+                var outStream = new MemoryStream();
+
+                image.Save(outStream, new JpegEncoder
+                {
+                    Quality = 85
+                });
+
+                return outStream;
+            }
+        }
+
+        public static Stream CreateThumbnail(Stream inStream, string contentType = null)
+        {
+            inStream.Position = 0;
+
+            using (var image = LoadImage(inStream, contentType))
+            {
+                var outStream = new MemoryStream();
+
+                const double maxDimension = 300.00;
+                var factor = maxDimension / Math.Max(image.Width, image.Height);
+
+                var newWidth = (int)(image.Width * factor);
+                var newHeight = (int)(image.Height * factor);
+
+                image
+                    .Mutate(c =>
+                        c.Resize(newWidth, newHeight, new Lanczos3Resampler())
+                    );
+
+                image.Save(outStream, new JpegEncoder
+                {
+                    Quality = 75
+                });
+
+                return outStream;
+            }
+        }
+
+        private static Image<Rgba32> LoadImage(Stream inStream, string contentType)
+        {
+            if (contentType == null)
+            {
+                return Image.Load(inStream);
+            }
+
+            return Image.Load(inStream, ResolveDecoderFromContentType(contentType));
+        }
+
+        private static IImageDecoder ResolveDecoderFromContentType(string contentType)
+        {
+            switch (contentType)
+            {
+                case "image/png":
+                    return new PngDecoder();
+                case "image/gif":
+                    return new GifDecoder();
+                case "image/bmp":
+                    return new BmpDecoder();
+                case "image/jpg":
+                case "image/jpeg":
+                    return new JpegDecoder();
+                default:
+                    return null;
+            }
+        }
+    }
+}
